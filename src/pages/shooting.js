9344e52c81e61ee32413b251aeb9c09100431945
LovisL <92515033+LovisL@users.noreply.github.com>
2025-09-22 12:58:54 +0200

    Link shooting

--- src/pages/shooting.js
+++ src/pages/shooting.js
@@ -1,16 +1,21 @@
-import {Grid, useMediaQuery} from "@mui/material";
+import {Grid, Stack, Typography, useMediaQuery} from "@mui/material";
 import HeaderMd from "@/components/index/HeaderMd";
 import HeaderSm from "@/components/index/HeaderSm";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import Zoom from "react-medium-image-zoom";
 
 export default function Shooting() {
     const isSm = useMediaQuery(theme => theme.breakpoints.down('md'))
+    const [data, setData] = useState([]);
     useEffect(() => {
         document.title = "Nicolas Edet - Shooting";
         axios.get('/api/sections/shootings')
-    },[])
+            .then(r => setData(Array.isArray(r.data) ? r.data : []))
+            .catch(e => console.log(e))
+    }, [])
     return (
+        <>
         <Grid container>
         {!isSm ? (
         <>
@@ -23,7 +28,26 @@ export default function Shooting() {
             <HeaderSm/>
         </Grid>
     )}
+            <Grid size={isSm ? 12 : 11}>
+            {data.map((shoot, index) => (
+                <Stack mt={2} key={index}>
+                    <Typography variant='h2'>{shoot.title}</Typography>
+                    <Grid container>
+                        {shoot.images.map((image, index) => (
+                            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                                <Zoom>
+                                    <img src={'/api/' +image} alt="" loading="lazy" />
+                                </Zoom>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Stack>
+            ))}
+            </Grid>
         </Grid>
+
+
+        </>
     )
 }
 
